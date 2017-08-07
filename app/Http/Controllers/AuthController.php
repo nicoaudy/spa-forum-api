b43d7dcd101d7986f4d06ba74965a859b56b9568
nicoaudy <nicoaudy1994@gmail.com>
2017-08-07 20:09:41 +0700

    signin request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Auth\RegisterFormRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
@@ -20,6 +21,20 @@ class AuthController extends Controller
 
     public function signin(Request $request)
     {
-        $token = JWTAuth::attempt($request->only('email', 'password'));
+        try {
+            $token = JWTAuth::attempt($request->only('email', 'password'));
+        } catch (JWTException $e) {
+            return response()->json([
+                'error' => 'Could not authenticate.'
+            ], 500);
+        }
+
+        if (!$token) {
+            return response()->json([
+                'error' => 'Could not authenticate.'
+            ], 401);
+        }
+
+        return response()->json(compact('token'));
     }
 }
