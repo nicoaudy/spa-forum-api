d263a5354ef17187f502bf1c160660e9e7b42b0f
nicoaudy <nicoaudy1994@gmail.com>
2017-08-07 20:06:15 +0700

    install & setup jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\RegisterFormRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
+use JWTAuth;
 
 class AuthController extends Controller
 {
@@ -14,5 +16,10 @@ class AuthController extends Controller
             'email'     => $request->json('email'),
             'password'  => bcrypt($request->json('password')),
         ]);
+    }
+
+    public function signin(Request $request)
+    {
+        $token = JWTAuth::attempt($request->only('email', 'password'));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,7 @@ Route::group(['middleware' => 'api'], function(){
     Route::post('auth/signup', [
         'uses' => 'AuthController@signup'
     ]);
+    Route::post('auth/signin', [
+        'uses' => 'AuthController@signin'
+    ]);
 });
